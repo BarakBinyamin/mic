879f0c8fbee98c943cdd27622da489c633015696
Barak Binyamin <50957106+BarakBinyamin@users.noreply.github.com>
2023-08-28 21:37:03 -0400

    Update platform.sh

--- platform.sh
+++ platform.sh
@@ -4,6 +4,8 @@ if  [[ "${platform}"  =~ "x86_64" ]]; then
     echo "linux/amd64"
 elif [[ "${platform}" =~ "arm64" ]]; then
     echo "linux/arm64"
+elif [[ "${platform}" =~ "armv7l" ]]; then
+    echo "linux/armv7l"
 else
     exit "Unkown"
 fi
